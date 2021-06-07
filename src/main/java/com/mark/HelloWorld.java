c38f35c3de0795d8e8c23a3c7497f094c91e907d
mark <mazheng0113@outlook.com>
2021-06-07 18:11:04 +0800

    Initial commit

---
+++ src/main/java/com/mark/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mark;
+
+/**
+ * @author Mark
+ * @date 2021/6/7 - 18:08
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
